ad02ca6017c1c60cc46330c8d1282f6e5983c5f1
Raziel Sotken <zromik@mail.ru>
2023-04-15 12:06:02 +0400

    В класс-помощник добавлены методы для выполнения GET и POST запросов.

--- helpers/rapidapihelper.py
+++ helpers/rapidapihelper.py
@@ -15,6 +15,7 @@ class RapidapiHelper:
         return instance
 
     def __internal_get_request(self, end_point: str, params: dict, headers: dict = None) -> 'Response':
+        """Выполнение GET-запроса к АПИ"""
         request_url = '/'.join(["https://hotels4.p.rapidapi.com/v2/get-meta-data", end_point])
         if headers:
             header = headers.copy()
@@ -28,5 +29,18 @@ class RapidapiHelper:
                 params=params,
                 url=request_url,
                 headers=header,
-                timeout=7.0
+                timeout=4.0
             )
+
+
+    def __internal_post_request(self, end_point: str, params: dict, headers: dict = None) -> None:
+        """Выполнение POST-запроса к АПИ"""
+        request_url = '/'.join(["https://hotels4.p.rapidapi.com/v2/get-meta-data", end_point])
+        if headers:
+            header = headers.copy()
+        else:
+            header = {
+                "content-type": "application/json",
+                "X-RapidAPI-Key": getenv("KEY"),
+                "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
+            }
