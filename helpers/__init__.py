c0ff283ad659b71988976100756eb4f8e3726c27
Raziel Sotken <zromik@mail.ru>
2023-04-15 14:08:22 +0400

    Оптимизация кода

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,1 +1,2 @@
 from helpers.db_helper import add_user, get_user_id, add_search
+from helpers.rapidapihelper import RapidapiHelper
--- helpers/rapidapihelper.py
+++ helpers/rapidapihelper.py
@@ -1,5 +1,7 @@
 import requests
 from os import getenv
+from typing import Optional, Any
+import json
 
 
 class RapidapiHelper:
@@ -8,15 +10,21 @@ class RapidapiHelper:
         """
         Реализация паттерна одиночка.
         """
-        instance = getattr(cls, "__instance")
+        instance = getattr(cls, "__instance", None)
         if not instance:
             instance = RapidapiHelper()
             setattr(cls, "__instance", instance)
         return instance
 
-    def __internal_get_request(self, end_point: str, params: dict, headers: dict = None) -> 'Response':
-        """Выполнение GET-запроса к АПИ"""
-        request_url = '/'.join(["https://hotels4.p.rapidapi.com/v2/get-meta-data", end_point])
+    def __internal_get_request(self, end_point: str, *,
+                               params: Optional[dict] = None, headers: Optional[dict] = None) -> Any:
+        """
+        Выполнение GET-запроса к АПИ
+        :param end_point: (str) точка запроса
+        :param params: (dict) Опционально. Параметры запроса
+        :param headers: (dict) Опционально. Заголовок запроса
+        """
+        request_url = '/'.join(["https://hotels4.p.rapidapi.com", end_point])
         if headers:
             header = headers.copy()
         else:
@@ -32,8 +40,7 @@ class RapidapiHelper:
                 timeout=4.0
             )
 
-
-    def __internal_post_request(self, end_point: str, params: dict, headers: dict = None) -> None:
+    def __internal_post_request(self, end_point: str, *, params: Optional[dict] = None, headers: Optional[dict] = None) -> Any:
         """Выполнение POST-запроса к АПИ"""
         request_url = '/'.join(["https://hotels4.p.rapidapi.com/v2/get-meta-data", end_point])
         if headers:
@@ -44,3 +51,19 @@ class RapidapiHelper:
                 "X-RapidAPI-Key": getenv("KEY"),
                 "X-RapidAPI-Host": "hotels4.p.rapidapi.com"
             }
+            return requests.request(
+                method='POST',
+                url=request_url,
+                json=params,
+                headers=header
+            )
+
+
+    def __is_good_response(self, data: 'Response'):
+        return data and data.status_code == 200
+    def get_metadata(self):
+        response = self.__internal_get_request(
+            end_point="v2/get-meta-data"
+        )
+        if self.__is_good_response(response):
+            return json.loads(response.text)
